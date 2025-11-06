e84e64287f45189592b12756ceec52e0e25f5f21
marcologioco <marco.logioco@edu-its.it>
2025-11-06 17:15:42 +0100

    Delete cine-verse/cine-verse/cine-verse directory

--- cine-verse/cine-verse/cine-verse/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function Footer() {
-    return
-    (<footer style={{ padding: '2rem', textAlign: 'center' }}>
-        <small>CineeVerse ©2025</small></footer>)
-
-
-}
